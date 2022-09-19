be5f057cee2afd425db5d035c89b4a073f6bae09
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-09-19 13:57:57 +0530

    Example of Relational Operator

---
+++ Operator1.java
@@ -0,0 +1,89 @@
+class Operator1
+{
+   public static void main(String centum[])
+    {
+       int a = 69, b = 36;
+
+    System.out.println("a is " + a + " and b is " + b);
+
+    System.out.println(a == b);
+
+    System.out.println(a != b);
+
+    System.out.println(a > b);
+
+    System.out.println(a < b);
+
+    System.out.println(a >= b);
+
+    System.out.println(a <= b);
+    }
+}
+
+
+/*class Operator1
+{
+   public static void main(String centum[])
+    {
+        int iNo1=45;
+        int iNo2=34;
+
+        if(iNo1==iNo2)
+        {
+            System.out.println("false");
+        }
+        else if(iNo1>iNo2)
+       {
+            System.out.println("iNo1 is greater");
+        }
+        else
+       {
+            System.out.println(" ");
+        }
+ }
+}*/
+
+
+/*class Operator1
+{
+  public static void main(String[] centum)
+  {
+
+    int iNo1 = 72, iNo2= 45;
+
+    System.out.println("Value of iNo1: " + iNo1);
+    iNo1++;
+    System.out.println("After increment: " +iNo1);
+
+    System.out.println("Value of iNo2: " + iNo2);
+    iNo2--;
+    System.out.println("After decrement: " + iNo2);
+  }
+}*/
+
+
+
+
+
+
+
+
+/*class Operator1
+{
+  public static void main(String[] centum)
+  {
+    int iNo = 6;
+    int value;
+
+    value = iNo;
+    System.out.println("value using = : " +value);
+
+    value += iNo;
+    System.out.println("value using += : " +value);
+
+    value *= iNo;
+    System.out.println("value using *= : " +value);
+  }
+}
+*/
+
