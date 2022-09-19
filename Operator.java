323b042e754f99ffd6993509355f148c443980ba
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-09-19 13:48:58 +0530

    example of equal to operator

---
+++ Operator.java
@@ -0,0 +1,17 @@
+class Operator
+{
+   public static void main(String args[])
+    {
+       String city = "Delhi";
+
+       if(city=="Delhi")
+       {
+        System.out.println("TRUE");
+       }
+       if(city!="Delhi")
+       {
+        System.out.println("FALSE");
+       }
+
+    }
+}
