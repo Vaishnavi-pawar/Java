f490457aaf894cf89603d26ecf84b8d66e42b59f
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-09-19 14:10:44 +0530

    Example of Relational Operator

---
+++ Operator2.java
@@ -0,0 +1,23 @@
+class Operator2
+{
+   public static void main(String centum[])
+    {
+       int a = 69, b = 36;
+
+    System.out.println("a is " + a + " and b is " + b);
+
+    System.out.println(a == b);
+
+    System.out.println(a != b);
+
+    System.out.println(a > b);
+
+    System.out.println(a < b);
+
+    System.out.println(a >= b);
+
+    System.out.println(a <= b);
+    }
+}
+
+
