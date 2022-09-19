e245be92fedf71b9190317b99eba5180be639911
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-09-19 20:54:36 +0530

    Example of if-else

---
+++ If1.java
@@ -0,0 +1,19 @@
+ class If1
+{
+    public static void main(String centum[])
+    {
+	int no1 = 30;
+	int no2 = 30;
+
+	if(no1>no2)
+	{
+	    System.out.println("no1 is greater");
+	    }
+	    else
+	    {
+	        System.out.println("no2 is greater");
+	    }
+
+    }
+
+}
