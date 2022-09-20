05f740b80998454fd3e843f816dcb58a9488c0c0
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-09-20 11:46:46 +0530

    Example of while loop

---
+++ Loop1.java
@@ -0,0 +1,14 @@
+class Loop1
+{
+    public static void main(String a[])
+    {
+        int i = 1;
+        int no = 6;
+
+        while(i<=10)
+        {
+            System.out.println(no + "*"+i+"="+no*i);
+            i++;
+        }
+    }
+}
