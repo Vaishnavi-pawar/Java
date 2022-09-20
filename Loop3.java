9164e09ed0a90804e53979a4c508b5f0d17987b9
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-09-20 11:51:09 +0530

    Example of For loop

---
+++ Loop3.java
@@ -0,0 +1,18 @@
+class Loop3
+{
+    public static void main(String a[])
+     {
+        int i ;
+        for(i=1;i<=10;i++)
+         {
+            if(i%2==0)
+            {
+                System.out.println(i + " is even no");
+            }
+            else
+            {
+                System.out.println(i + " is odd no");
+            }
+          }
+     }
+}
