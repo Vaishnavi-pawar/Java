9953ba003f418cd3c60a72ec86df1135098fcd56
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-09-25 19:36:52 +0530

    methods in java

---
+++ Method1.java
@@ -0,0 +1,15 @@
+class Method1
+{
+    int add(int no1 , int no2)
+    {
+        int sum = no1+no2;
+        return sum;
+    }
+    public static void main(String a[])
+    {
+        Method1 hello = new Method1();
+        int res = hello.add(1,9);
+        System.out.println(res);
+    }
+    System.out.println(hello.add(1,9));
+}
