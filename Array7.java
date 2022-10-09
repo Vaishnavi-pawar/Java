ec5aac1adf2026097bbca9078a65372dd4beafb6
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-10-09 18:41:08 +0530

    Add files via upload

---
+++ Array7.java
@@ -0,0 +1,106 @@
+class Array7
+{
+  public static void main(String yahoo[])
+  {
+   String firstName =  yahoo[0];
+    String secondName =  yahoo[1];
+
+    System.out.println(yahoo[0]+" "+yahoo[1]);
+    System.out.println(firstName+" "+secondName);
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*void display(int arr[])
+
+int no[];
+no[]=new int[]
+no[1]=5;
+no[2]=4;
+no[3]=7;
+display(no)*/
+
+/*class Array6
+{
+  public static void main(String yahoo[])
+  {
+   String firstName =  yahoo[0];
+    String secondName =  yahoo[1];
+
+    System.out.println(yahoo[0]+" "+yahoo[1]);
+    System.out.println(firstName+" "+secondName);
+    //javac Array6.java
+    // java Array6 ani ith don nav takayche(java Array vaish pawar)
+  }
+}*/
+/*class Array6
+{
+  public static void main(String yahoo[])
+  {
+  // String str[]=yahoo;
+
+    for(int i=0; i<yahoo.length;i++)
+    {
+        System.out.println(yahoo[i]);
+    }
+  }
+}*/
+/*class Array6
+{
+  public static void main(String yahoo[])
+  {
+   //String str[]=yahoo;
+
+    int no1[]={1,5,3,6};
+    int no2[]=no1;
+
+    for(int i=0; i<no2.length;i++)
+    {
+        System.out.println(no2[i]);
+    }
+  }
+}*/
+/*class Array6
+{
+  public static void main(String yahoo[])
+  {
+   // int no1 = yahoo[0];  types: String cannot be converted to int
+  int no1 =Integer.parseInt(yahoo[0]);
+   int no2 =Integer.parseInt(yahoo[1]);
+
+    int sum = no1+no2;
+    System.out.println("Sum = " +sum);
+//javac Array6.java
+//java Array6 10 20
+//Sum = 30
+  }
+}*/
