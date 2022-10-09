458e158bf4bbd73416815df59a6d0a940b81fb2a
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-10-09 18:40:12 +0530

    Add files via upload

---
+++ Array4.java
@@ -0,0 +1,36 @@
+/*class Array1
+{
+  public static void main(String a[])
+  {
+    int []arr[]={{2,1,4}, {4,5,6,8}};
+
+    for(int i=0; i<arr.length; i++)
+    {
+    for(int j=0; j<arr.length; j++)
+    {
+        System.out.print(arr[i][j]+" ");
+    }
+        System.out.println( );
+  }
+    }
+}*/
+
+class Array1
+{
+  public static void main(String a[])
+  {
+    int []arr[]={{2,1,4}, {4,5,6,8},{3,4}};
+
+    for(int i=0; i<arr.length; i++)
+    {
+    for(int j=0; j<arr.length; j++)
+    {
+        System.out.print(arr[i][j]+" ");
+    }
+        System.out.println( );
+  }
+    }
+}
+
+
+
