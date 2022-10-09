52a0c9e64fe65d53377a15bece49f8b72fd2f62c
Vaishnavi Pawar <106009073+Vaishnavi-pawar@users.noreply.github.com>
2022-10-09 18:48:22 +0530

    Array4.java

--- Array4.java
+++ Array4.java
@@ -1,19 +1,3 @@
-/*class Array1
-{
-  public static void main(String a[])
-  {
-    int []arr[]={{2,1,4}, {4,5,6,8}};
-
-    for(int i=0; i<arr.length; i++)
-    {
-    for(int j=0; j<arr.length; j++)
-    {
-        System.out.print(arr[i][j]+" ");
-    }
-        System.out.println( );
-  }
-    }
-}*/
 
 class Array1
 {
@@ -31,6 +15,4 @@ class Array1
   }
     }
 }
-
-
 
